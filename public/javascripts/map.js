45b1416a9ea8d681fcf58234163a1b21210de383
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2016-10-17 23:39:21 -0500

    add new york organizations

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -46,6 +46,144 @@ var geojson = {
                 "marker-size": "large",
                 "marker-symbol": "star"
             }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -74.005941,
+                    40.712784
+                ]
+            },
+            "properties": {
+                "title": "New York, NY",
+                "organizations": [
+                    {
+                        "name": "National Latina Institute for Reproductive Justice",
+                        "engagementLevel": 1,
+                        "members": ["Felipe", "Claudia"]
+                    },
+                    {
+                        "name": "National Domestic Worker's Alliance",
+                        "engagementLevel": 1,
+                        "members": ["Felipe", "Claudia"]
+                    },
+                    {
+                        "name": "Intereligious Foundation Community Organization",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "New York Collective of Radical Educators",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "New York Nurse Association",
+                        "engagementLevel": 3,
+                        "members": ["Stephanie"]
+                    },
+                    {
+                        "name": "Black Alliance for Just Immigration",
+                        "engagementLevel": 1,
+                        "members": ["Felipe", "Rahiel"]
+                    },
+                    {
+                        "name": "Malcolm X Grassroots",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "El Grito Sunset Park",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "Justice Committee",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "Existance is Resistance",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "People Power Movement",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "Eztudio 43",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "Rockaway Youth Taskforce",
+                        "engagementLevel": 1,
+                        "members": ["Rahiel"]
+                    },
+                    {
+                        "name": "NY Justice League",
+                        "engagementLevel": 1,
+                        "members": ["Rahiel"]
+                    },
+                    {
+                        "name": "SEIU 32 BJ",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    },
+                    {
+                        "name": "Revolutionary Student Coordination Committee",
+                        "engagementLevel": 1,
+                        "members": ["Kevin"]
+                    },
+                    {
+                        "name": "MinkWN",
+                        "engagementLevel": 1,
+                        "members": ["Kevin"]
+                    },
+                    {
+                        "name": "DRUM ",
+                        "engagementLevel": 1,
+                        "members": ["Kevin"]
+                    },
+                    {
+                        "name": "Families for Freedom",
+                        "engagementLevel": 1,
+                        "members": ["Kevin"]
+                    },
+                    {
+                        "name": "Brotherhood Sister Soul",
+                        "engagementLevel": 1,
+                        "members": ["Claudia", "Rahiel"]
+                    },
+                    {
+                        "name": "Make the Road",
+                        "engagementLevel": 1,
+                        "members": ["Kevin","Claudia","Felipe"]
+                    },
+                    {
+                        "name": "API for BlackLives",
+                        "engagementLevel": 1,
+                        "members": ["Kevin"]
+                    },
+                    {
+                        "name": "Queer Detenee Empowerment Project",
+                        "engagementLevel": 1,
+                        "members": ["Felipe"]
+                    },
+                    {
+                        "name": "Audre Lorde Project",
+                        "engagementLevel": 1,
+                        "members": ["Claudia"]
+                    }
+                ],
+                "marker-color": "#f86767",
+                "marker-size": "large",
+                "marker-symbol": "star"
+            }
         }
     ]
 };
@@ -71,5 +209,5 @@ map.on('move', empty);
 empty();
 
 function empty() {
-    info.innerHTML = '<div><strong>Organizations</strong></div>';
+    info.innerHTML = '<div></div>';
 }
